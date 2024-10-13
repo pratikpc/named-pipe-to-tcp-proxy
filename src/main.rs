77e1cb3da2fb4f10047d3141f8ef7a8596bd8f77
Pratik <28534575+pratikpc@users.noreply.github.com>
2024-10-13 16:23:12 +0530

    feat(main/Splice::Readable): implement trait for tcp stream and named pipe server
    Needed for integration with Splice

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,36 @@
 mod named_pipe_server;
 mod splice;
+
+use splice::splice;
+use tokio::{
+    io,
+    net::{windows::named_pipe::NamedPipeServer, TcpStream},
+};
+
+/// Implement the `TryRead` trait for our structures  
+/// Remember that this is just needed because Tokio
+/// does not provide traits for the below functions
+impl splice::Readable for NamedPipeServer {
+    fn try_read(&self, buf: &mut [u8]) -> io::Result<usize> {
+        Self::try_read(self, buf)
+    }
+    async fn readable(&self) -> io::Result<()> {
+        Self::readable(self).await
+    }
+}
+
+/// Implement the `TryRead` trait for our structures  
+/// Remember that this is just needed because Tokio
+/// does not provide traits for the below functions
+impl splice::Readable for TcpStream {
+    fn try_read(&self, buf: &mut [u8]) -> io::Result<usize> {
+        return Self::try_read(self, buf);
+    }
+    async fn readable(&self) -> io::Result<()> {
+        Self::readable(self).await
+    }
+}
+
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
     println!("Hello, world!");
